901e6147af99db69d63738bf735d2599ce192e39
denzildoyle <yell@denzildoyle.me>
2015-02-19 22:40:11 -0400

    Leave splash screen up untill app is ready so there witll be no flickering in video

--- background-video/www/js/app.js
+++ background-video/www/js/app.js
@@ -9,6 +9,10 @@ angular.module('starter', ['ionic'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+    setTimeout(function() {
+        navigator.splashscreen.hide();
+    }, 100);
+
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
